1cf4b9ad11c54d88aec912339e8e7899823b919f
yakovlev <ivanvyakovlev@gmail.com>
2018-04-10 19:57:45 +0300

    added npm scripts

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express      = require('express');
 const path         = require('path');
-const favicon      = require('serve-favicon');
 const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
-
-const index = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
 
@@ -15,15 +11,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
