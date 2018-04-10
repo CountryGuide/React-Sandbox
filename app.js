509e8f9d552d6da4c942005334ab07d2a6f41ff7
yakovlev <ivanvyakovlev@gmail.com>
2018-04-10 20:08:47 +0300

    deleted view engine

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 
+
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+require('./routes/index')(app);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
@@ -29,17 +27,6 @@ app.use(function (req, res, next) {
     const err  = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error   = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const router  = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = (app) => {
+    app.get(
+        '/api',
+        (req, res) => {
+            res.send('Hello from API');
+        }
+    );
+};
