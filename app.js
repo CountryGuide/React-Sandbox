5f56e5c8ac24db881bf05db97dd3dcb3d807aba2
yakovlev <ivanvyakovlev@gmail.com>
2018-04-11 17:47:55 +0300

    FEAT: Authorization

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ const express      = require('express');
 const path         = require('path');
 const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const passport      = require('passport');
 const bodyParser   = require('body-parser');
+const mongoose      = require('mongoose');
+const keys          = require('./config/keys');
 
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
 
 const app = express();
 
@@ -11,9 +22,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 3600 * 1000,
+        keys:   [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/index')(app);
+require('./routes/auth')(app);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
