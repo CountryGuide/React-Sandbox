5e7c42d24fd315781ec8fd9d52004f55d3bb02e5
Ivan Yakovlev <ivanvyakovlev@gmail.com>
2018-04-18 22:53:52 +0300

    styling

--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -6,28 +6,26 @@ import '../style/Aside.css';
 
 
 export class SideMenu extends React.Component {
-    render() {
+    render () {
         return (
             <aside className="side-menu">
-                <div className="uk-flex uk-flex-center uk-flex-middle">
-                    <Link to="/" className="uk-logo uk-navbar-item">
-                        <span data-uk-icon="home"></span>
-                    </Link>
-                </div>
-                <div className="uk-flex uk-flex-center uk-flex-middle">
-                    <Link to="/profile" className="uk-logo uk-navbar-item">
-                        <span data-uk-icon="user"></span>
-                    </Link>
-                </div>
-                <div className="uk-flex uk-flex-center uk-flex-middle">
-                    <span data-uk-icon="cloud-upload"></span>
-                </div>
-                <div className="uk-flex uk-flex-center uk-flex-middle">
-                    <span data-uk-icon="github-alt"></span>
-                </div>
-                <div className="uk-flex uk-flex-center uk-flex-middle">
-                    <span data-uk-icon="database"></span>
-                </div>
+                <Link to="/"
+                      className="uk-flex side-menu__item uk-flex-center uk-flex-middle">
+                    <span data-uk-icon="home"></span>
+                </Link>
+                <Link to="/profile"
+                      className="uk-flex side-menu__item uk-flex-center uk-flex-middle">
+                    <span data-uk-icon="user"></span>
+                </Link>
+                <span data-uk-icon="cloud-upload"
+                      className="uk-flex side-menu__item uk-flex-center uk-flex-middle">
+                </span>
+                <span data-uk-icon="github-alt"
+                      className="uk-flex side-menu__item uk-flex-center uk-flex-middle">
+                </span>
+                <span data-uk-icon="database"
+                      className="uk-flex side-menu__item uk-flex-center uk-flex-middle">
+                </span>
             </aside>
         );
     }
